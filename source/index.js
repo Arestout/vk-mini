07b6b4bb0b9f00d2ac17ef0209fb78be551cce98
Anton Kablukov <antoha7@gmail.com>
2020-09-28 12:20:13 +0200

    test

--- source/index.js
+++ source/index.js
@@ -11,6 +11,4 @@ import './db';
 // const PORT = getPort() || 5000;
 const dg = debug('server:main');
 
-app.listen(process.env.PORT || 5000, () => {
-  dg(`Server API is running on port ${process.env.PORT}`);
-});
+app.listen(process.env.PORT || 5000);
